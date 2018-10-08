a68987df9d4c2da75dc014a14b41dc270c8c9c1d
Ghulam mohiyuddin <subahani98@gmail.com>
2018-10-08 11:08:58 +0530

    Update LispBasic.lisp

--- LispBasic.lisp
+++ LispBasic.lisp
@@ -14,3 +14,6 @@
 (write (rest(first(rest '((a b)(c d))))))
 (write-line "")
 (write (first(rest(first '((a b)(c d))))))
+(write(position 2 '(6 7 8 2 3 4)));find index
+
+(print (nth 3 '(a b c d)) );print index value
